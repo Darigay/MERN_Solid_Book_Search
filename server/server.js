ea129b4c7e1e2aa39eaa5bf7c1f88494a86343d7
Divya <divya.arigay@gmail.com>
2022-05-18 19:20:11 -0600

    Heroku Deployment Fixed

--- server/server.js
+++ server/server.js
@@ -10,9 +10,7 @@
 // app.use(express.json());
 
 // // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+
 
 //  app.use(routes);
 
@@ -20,11 +18,16 @@
 //   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 // });
 const express = require('express');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
+ if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+ }
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
